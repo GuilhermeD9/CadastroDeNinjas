393db0e9bbdaf1a963a17d04a84f02987ef6be8c
Guilherme Silva <sgsouza.173@gmail.com>
2025-02-08 13:36:00 -0300

    feat: Adicionado o front-end da tabela de Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesControllerUi.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesControllerUi.java
@@ -1,10 +1,10 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,6 +21,37 @@ public class MissoesControllerUi {
     public String listarMissao(Model model) {
         List<MissoesDTO> missoes = missoesService.listarMissoes();
         model.addAttribute("missoes", missoes);
-        return "ListarMissoes";
+        return "listarMissoes";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarMissaoPorId(@PathVariable Long id) {
+        missoesService.deletarMissaoPorId(id);
+        return "redirect:/missoes/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarMissoesPorID(@PathVariable Long id, Model model) {
+        MissoesDTO missao = missoesService.listarMissaoPorId(id);
+        if (missao != null) {
+            model.addAttribute("missao", missao);
+            return "detalhesMissao";
+        } else {
+            model.addAttribute("mensagem", "Missão não encontrada!");
+            return "listarMissoes";
+        }
+    }
+
+    @GetMapping("/adicionar")
+    public String mostrarFormularioDeAdicao(Model model) {
+        model.addAttribute("missao", new MissoesDTO());
+        return "adicionarMissao";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute MissoesDTO missao, RedirectAttributes redirectAttributes) {
+        missoesService.criarMissao(missao);
+        redirectAttributes.addFlashAttribute("mensagem", "Missão criada com sucesso!");
+        return "redirect:/missoes/ui/listar";
     }
 }
